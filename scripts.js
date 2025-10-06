686a1778bea761e79448dd04014dd7387382db0a
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-06 20:41:34 -0300

    Corrections in the script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 function enviarMensagem(event){
     event.preventDefault()
-    console.log(event)
     let nome = document.querySelector("#nome").value;
     let mensagem = document.querySelector("#mensagem").value;
     const telefone = "555492415580";
@@ -9,7 +8,6 @@ function enviarMensagem(event){
     const mensagemFormatada = encodeURIComponent(texto);
 
     const url = `https://wa.me/${telefone}?text=${mensagemFormatada}`;
-    console.log(url);
     window.open(url, "_blank");
     nome.value = "";
     mensagem.value = "";
